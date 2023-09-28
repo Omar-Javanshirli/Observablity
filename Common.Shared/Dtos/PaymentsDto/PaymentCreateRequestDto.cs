91779c6726c215e4a7e326c7a1ef68b50e81e5ff
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-09-28 13:44:09 -0700

    new update

---
+++ Common.Shared/Dtos/PaymentsDto/PaymentCreateRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Common.Shared.Dtos.PaymentsDto
+{
+    public record PaymentCreateRequestDto
+    {
+        public string OrderCode { get; set; } = null!;
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ Payment.API/Controllers/PaymentProccesController.cs
@@ -0,0 +1,22 @@
+﻿using Common.Shared.Dtos;
+using Common.Shared.Dtos.PaymentsDto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Payment.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PaymentProccesController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Create(PaymentCreateRequestDto request)
+        {
+            const decimal balance = 1000;
+
+            if (request.TotalPrice > balance)
+                return BadRequest(ResponseDto<string>.Fail(400, "balansda kifayet qeder vasait yoxdur"));
+
+            return Ok(ResponseDto<string>.Success(200,"kart prosesi ugurla heyata kecmisdir"));
+        }
+    }
+}
